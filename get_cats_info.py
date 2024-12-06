9e64abc2c75ff863c4227fb6fc83d66e89621597
TheOneWhoIsADemon <nyashnyclown1998@gmail.com>
2024-12-06 12:50:42 +0100

    Додано розв'язання домашнього завдання

--- get_cats_info.py
+++ get_cats_info.py
@@ -1,25 +1,29 @@
+# Створюємо фунцію
 def get_cats_info(path):
     try:
+        # Відриваємо файл для читання
         with open(path, 'r', encoding='utf-8') as file:
+            # Пустий список для збереження інформації
             cats = []
-
+            # Зчитуємо файл
             for line in file:
+                # Видаляємо зайві пробіли і розділові знаки
                 parts = line.strip().split(',')
-
+                # Робимо три елементи
                 if len(parts) == 3:
                     try:
                         cat_info = {
                             "id": parts[0],
                             "name": parts[1],
-                            "age": int(parts[2])
+                            "age": int(parts[2]) # Перетворюємо вік у чисо
                         }
-                        cats.append(cat_info)
+                        cats.append(cat_info) # Додаємо у пустий список cats
 
                     except ValueError:
                         print(f"Помилка: Вік кота не є числом у рядку '{line.strip()}'")
 
             return cats
-
+    # помилки які можуть бути
     except FileNotFoundError:
         print(f"Файл за шляхом '{path}' не знайдено.")
         return []
@@ -27,6 +31,6 @@ def get_cats_info(path):
     except Exception as e:
         print(f"Виникла помилка: {e}")
         return []
-
+# Приклад
 cats_info = get_cats_info("cet.txt")
 print(cats_info)
--- total_salary.py
+++ total_salary.py
@@ -1,24 +1,27 @@
+# Створюємо функцію
 def total_salary(path):
     try:
-
+        # Вілкриваємо файл для читання
         with open(path, 'r', encoding='utf-8') as file:
+            # Створюємо пустий список для зберігання зарплати
             salaries = []
 
+            # Зчитужмо файт
             for line in file:
-
+                # Видаляємо зайві пробіли та розділяємо рядокк на прізвище і зарплату
                 parts = line.strip().split(',')
                 if len(parts) == 2:
                     try:
-
+                        # Додаємо зарплату до списку та перетворюємо в число
                         salaries.append(float(parts[1]))
                     except ValueError:
                         print(f"Помилка в обробці рядка: {line.strip()} - зарплата не є числом.")
 
-
+            # Преревіряємо чи є зарплата
             if not salaries:
                 return (0, 0)
 
-
+            # Розраховуємо загальну та середню зарплату
             total = sum(salaries)
             average = total / len(salaries)
             return (total, average)
@@ -30,6 +33,6 @@ def total_salary(path):
         return (0, 0)
 
 
-
+# Приклад
 total, average = total_salary("salary.txt")
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
